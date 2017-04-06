60108fe0564a86960b341474f651b1d2f1ec369a
mitochondrion <mitochondrion@users.noreply.github.com>
2017-04-05 20:13:45 -0400

    Example App WebViewActivity now manually launches oAuthCallbackUrl by overriding WebView.setWebViewClient

--- exampleApp/src/main/java/it/trade/android/exampleapp/WebViewActivity.java
+++ exampleApp/src/main/java/it/trade/android/exampleapp/WebViewActivity.java
@@ -2,10 +2,13 @@ package it.trade.android.exampleapp;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import static it.trade.android.exampleapp.OauthLinkBrokerActivity.OAUTH_URL_PARAMETER;
 
@@ -22,6 +25,20 @@ public class WebViewActivity extends AppCompatActivity {
         WebView myWebView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+                return true;
+            }
+        });
+
         myWebView.loadUrl(urlParam);
     }
 }
