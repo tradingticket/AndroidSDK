55f64c0fbcbc039e2897ca7317f4ff3d177dde23
Debavelaere <guillaume.debavelaere@gmail.com>
2018-09-03 09:22:46 +0300

    remove unused tests

--- tradeit-android-sdk/src/androidTest/kotlin/it/trade/android/sdk/ApplicationTest.kt
+++
@@ -1,9 +0,0 @@
-package it.trade.android.sdk
-
-import android.app.Application
-import android.test.ApplicationTestCase
-
-/**
- * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
- */
-class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
